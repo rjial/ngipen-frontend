2ad2b5ad15e1d597c1510f689e247d3fa17f7d0d
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2024-04-18 01:43:51 +0700

    add carousel event

---
+++ app/components/home/ImageGallery.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
+
+const carouselSrc = [
+    "https://source.unsplash.com/JNuKyKXLh8U",
+    "https://source.unsplash.com/kcJsQ3PJrYU",
+    "https://source.unsplash.com/Q_KdjKxntH8",
+    "https://source.unsplash.com/U7HLzMO4SIY",
+    "https://source.unsplash.com/LETdkk7wHQk",
+  ]
+export const ImageGallery: React.FC = () => (<Carousel className="w-full max-w-full h-96">
+    <CarouselContent className="h-96">
+      {carouselSrc.map((item, index) => (
+        <CarouselItem key={index} className="h-96">
+          <div className="p-0 h-96">
+            <Card className="h-96">
+              <CardContent className="flex items-center w-full justify-center p-0 rounded-lg h-96">
+                <img src={item} alt="lorem ipsum" className="w-full rounded-lg h-full object-cover" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      ))}
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,11 @@ import { PopularEvent } from "~/components/home/PopularEvent";
 import { UserClaim } from "~/data/entity/auth/UserClaim";
 import { Event } from "~/data/entity/events/Event";
 import { IEventService } from "~/service/events/IEventService.server";
-import {typedjson, useTypedLoaderData} from "remix-typedjson"
+import { typedjson, useTypedLoaderData } from "remix-typedjson"
 import { getUserClaim } from "~/utils/authUtil";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import { ImageGallery } from "~/components/home/ImageGallery";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,19 +24,21 @@ export interface IndexPageLoaderType {
   user: TypedResponse<UserClaim | undefined>
 }
 
-export async function loader({ request}: LoaderFunctionArgs) {
+export async function loader({ request }: LoaderFunctionArgs) {
   const eventService = new IEventService();
   const res = await eventService.getEvents();
   const user = await getUserClaim(request)
   console.log(user)
   const events: Event[] = res.data as Event[]
-  return json({events, user})
+  return json({ events, user })
 }
+
 export default function Index() {
-  const {user} = useLoaderData<typeof loader>() 
+  const { user } = useLoaderData<typeof loader>()
   return (
-    <div className="px-24">
-      <NavBar user={user}/>
+    <div className="px-24 space-y-3">
+      <NavBar user={user} />
+      <ImageGallery />
       <PopularEvent />
     </div>
   );
