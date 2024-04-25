1f030b9873e6c09fe13d2fc2c5e6a6c3a9c7b421
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2024-04-25 20:12:16 +0700

    fix dashboard icon

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -9,6 +9,7 @@ import { PropsWithChildren, useState } from "react"
 import { NavBar, NavbarUser } from "~/components/common/Navbar"
 import { UserClaim } from "~/data/entity/auth/UserClaim"
 import { cn } from "@/lib/utils"
+import { Bell, ChevronRight, Home, Menu, Package, Package2, Search, Settings, ShoppingCart, User } from "lucide-react"
 
 export type DashboardMenuCollapsibleProp = {
     title: string,
@@ -35,9 +36,9 @@ const DashboardMenuCollapsible: React.FC<PropsWithChildren<DashboardMenuCollapsi
     return (
         <Collapsible open={open} onOpenChange={(opened) => setOpen(opened)} className="grid gap-2">
             <CollapsibleTrigger className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 [&[data-state=open]>svg]:rotate-90">
-                <PackageIcon className="h-4 w-4" />
+                <Package size={16} />
                 {props.title}
-                <ChevronRightIcon className="ml-auto h-4 w-4 transition-transform" />
+                <ChevronRight className="ml-auto transition-transform" size={16} />
             </CollapsibleTrigger>
             <CollapsibleContent>
                 {props.children}
@@ -72,15 +73,15 @@ const DashboardMenu: React.FC = () => {
     return (
         <div className="flex-1 overflow-auto py-2">
             <nav className="grid items-start px-4 text-sm font-medium">
-                <DashboardMenuItem title="Home" icon={<HomeIcon className="h-4 w-4" />} isActive={false} />
-                <DashboardMenuItem title="Users" icon={<UsersIcon className="h-4 w-4" />} isActive={true} />
-                <DashboardMenuItem title="Product" icon={<PackageIcon className="h-4 w-4" />} isActive={false}>
-                    <DashboardMenuCollapsible title="Product Management" icon={<PackageIcon className="h-4 w-4" />}>
-                        <DashboardMenuCollapsibleItem icon={<PackageIcon className="h-4 w-4" />} title="All Products" />
+                <DashboardMenuItem title="Home" icon={<Home size={16} />} isActive={false} />
+                <DashboardMenuItem title="Users" icon={<User size={16} />} isActive={true} />
+                <DashboardMenuItem title="Product" icon={<Package size={16} />} isActive={false}>
+                    <DashboardMenuCollapsible title="Product Management" icon={<Package size={16} />}>
+                        <DashboardMenuCollapsibleItem icon={<Package size={16} />} title="All Products" />
                     </DashboardMenuCollapsible>
                 </DashboardMenuItem>
-                <DashboardMenuItem title="Orders" icon={<ShoppingCartIcon className="h-4 w-4" />} isActive={false} />
-                <DashboardMenuItem title="Settings" icon={<SettingsIcon className="h-4 w-4" />} isActive={false} />
+                <DashboardMenuItem title="Orders" icon={<ShoppingCart size={16} />} isActive={false} />
+                <DashboardMenuItem title="Settings" icon={<Settings size={16} />} isActive={false} />
             </nav>
         </div>
     )
@@ -94,7 +95,7 @@ const useDashboardMobileSheet = () => {
                 <div className="flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-[60px] items-center border-b px-6">
                         <Link className="flex items-center gap-2 font-semibold" to="#">
-                            <Package2Icon className="h-6 w-6" />
+                            <Package2 size={16} />
                             <span className="">Acme Inc</span>
                         </Link>
                     </div>
@@ -104,14 +105,14 @@ const useDashboardMobileSheet = () => {
                                 className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
                                 to="#"
                             >
-                                <HomeIcon className="h-4 w-4" />
+                                <Home size={16} />
                                 Home
                             </Link>
                             <Link
                                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                                 to="#"
                             >
-                                <UsersIcon className="h-4 w-4" />
+                                <User size={16} />
                                 Users
                             </Link>
                             <div className="grid gap-2">
@@ -119,25 +120,25 @@ const useDashboardMobileSheet = () => {
                                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                                     to="#"
                                 >
-                                    <PackageIcon className="h-4 w-4" />
+                                    <Package size={16}/>
                                     Products
                                 </Link>
-                                <DashboardMenuCollapsible title="Product Management" icon={<PackageIcon className="h-4 w-4" />}>
-                                    <DashboardMenuCollapsibleItem icon={<PackageIcon className="h-4 w-4" />} title="All Products" />
+                                <DashboardMenuCollapsible title="Product Management" icon={<Package size={16} />}>
+                                    <DashboardMenuCollapsibleItem icon={<Package size={16} />} title="All Products" />
                                 </DashboardMenuCollapsible>
                             </div>
                             <Link
                                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                                 to="#"
                             >
-                                <ShoppingCartIcon className="h-4 w-4" />
+                                <ShoppingCart size={16} />
                                 Orders
                             </Link>
                             <Link
                                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                                 to="#"
                             >
-                                <SettingsIcon className="h-4 w-4" />
+                                <Settings size={16}/>
                                 Settings
                             </Link>
                         </nav>
@@ -158,11 +159,11 @@ export default function DashboardLayout() {
                 <div className="flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-[60px] items-center border-b px-6">
                         <Link className="flex items-center gap-2 font-semibold" to="#">
-                            <Package2Icon className="h-6 w-6" />
+                            <Package2 size={16} />
                             <span className="">Ngipen</span>
                         </Link>
                         <Button className="ml-auto h-8 w-8" size="icon" variant="outline">
-                            <BellIcon className="h-4 w-4" />
+                            <Bell className="h-4 w-4" />
                             <span className="sr-only">Toggle notifications</span>
                         </Button>
                     </div>
@@ -172,7 +173,7 @@ export default function DashboardLayout() {
             <div className="flex flex-col">
                 <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
                     <Button className="lg:hidden" size="icon" variant="outline" onClick={() => handleSheet()}>
-                        <MenuIcon className="h-6 w-6" />
+                        <Menu size={16} />
                         <span className="sr-only">Toggle navigation menu</span>
                     </Button>
                     <div className="flex-1">
@@ -181,7 +182,7 @@ export default function DashboardLayout() {
                     <div className="flex flex-1 items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
                         <form className="ml-auto flex-1 sm:flex-initial">
                             <div className="relative">
-                                <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
+                                <Search className="absolute left-2.5 top-2.5 text-gray-500 dark:text-gray-400" />
                                 <Input
                                     className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px] bg-white"
                                     placeholder="Search orders..."
@@ -198,220 +199,5 @@ export default function DashboardLayout() {
                 </main>
             </div>
         </div>
-    )
-}
-
-function BellIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-            <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-        </svg>
-    )
-}
-
-
-function ChevronRightIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m9 18 6-6-6-6" />
-        </svg>
-    )
-}
-
-
-function HomeIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-            <polyline points="9 22 9 12 15 12 15 22" />
-        </svg>
-    )
-}
-
-
-function MenuIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <line x1="4" x2="20" y1="12" y2="12" />
-            <line x1="4" x2="20" y1="6" y2="6" />
-            <line x1="4" x2="20" y1="18" y2="18" />
-        </svg>
-    )
-}
-
-
-function Package2Icon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z" />
-            <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9" />
-            <path d="M12 3v6" />
-        </svg>
-    )
-}
-
-
-function PackageIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m7.5 4.27 9 5.15" />
-            <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-            <path d="m3.3 7 8.7 5 8.7-5" />
-            <path d="M12 22V12" />
-        </svg>
-    )
-}
-
-
-function SearchIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <circle cx="11" cy="11" r="8" />
-            <path d="m21 21-4.3-4.3" />
-        </svg>
-    )
-}
-
-
-function SettingsIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-            <circle cx="12" cy="12" r="3" />
-        </svg>
-    )
-}
-
-
-function ShoppingCartIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <circle cx="8" cy="21" r="1" />
-            <circle cx="19" cy="21" r="1" />
-            <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-        </svg>
-    )
-}
-
-
-function UsersIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-            <circle cx="9" cy="7" r="4" />
-            <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-            <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-        </svg>
     )
 }
